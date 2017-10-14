4a608b2c2ab824e88199e3f242e3e7136a9abfc1
JosXa <joscha.goetzer@gmail.com>
2017-10-15 00:49:51 +0200

    Disabling botchecker

--- settings.py
+++ settings.py
@@ -42,8 +42,8 @@ SUGGESTION_LIMIT = 25
 API_URL = "localhost" if DEV else "josxa.jumpingcrab.com"
 API_PORT = 6060
 
-USERBOT_SESSION = "/home/joscha/accounts/lulzx"
-RUN_BOTCHECKER = True
+USERBOT_SESSION = "/home/joscha/accounts/+380686087314"
+RUN_BOTCHECKER = False
 
 ERROR_LOG_FILE = os.path.join(LOG_DIR, "error.log")
 DEBUG_LOG_FILE = os.path.join(LOG_DIR, "bot.log")
